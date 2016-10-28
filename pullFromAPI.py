f50c6f3ded47e2684130cbdaa78a880ec5cb52a1
Joseph Haaga <joseph@Joseph-Dev-MBP-R-242.local>
2016-10-28 09:32:18 -0400

    pull data

--- pullFromAPI.py
+++ pullFromAPI.py
@@ -1,40 +1,22 @@
-########### Python 2.7 #############
-import httplib, urllib, base64
-
-headers = {
-    # Request headers
-    'api_key': '{subscription key}',
-}
-
-params = urllib.urlencode({
-})
-
-try:
-    conn = httplib.HTTPSConnection('api.wmata.com')
-    conn.request("GET", "/TrainPositions/StandardRoutes?contentType={contentType}&%s" % params, "{body}", headers)
-    response = conn.getresponse()
-    data = response.read()
-    print(data)
-    conn.close()
-except Exception as e:
-    print("[Errno {0}] {1}".format(e.errno, e.strerror))
-
-####################################
-
 ########### Python 3.2 #############
 import http.client, urllib.request, urllib.parse, urllib.error, base64
 
+keys = open('keys.py');
+key = keys.readline().replace('\n','');
+
 headers = {
     # Request headers
-    'api_key': '{subscription key}',
+    'api_key': key,
 }
 
 params = urllib.parse.urlencode({
+    
 })
 
 try:
     conn = http.client.HTTPSConnection('api.wmata.com')
-    conn.request("GET", "/TrainPositions/StandardRoutes?contentType={contentType}&%s" % params, "{body}", headers)
+    # conn.request("GET", "/TrainPositions/StandardRoutes?contentType={contentType}&%s" % params, "{body}", headers)
+    conn.request("GET", "/TrainPositions/StandardRoutes?contentType=json&%s" % params, "{body}", headers)
     response = conn.getresponse()
     data = response.read()
     print(data)
